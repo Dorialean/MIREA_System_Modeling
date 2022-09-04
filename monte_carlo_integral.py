33ffee78a6e54fd4debe882b51f88fcaf67917a5
dmitry <lana.lakutina@mail.ru>
2022-09-04 20:39:14 +0300

    works

--- monte_carlo_integral.py
+++ monte_carlo_integral.py
@@ -1,4 +1,7 @@
+import math
 import random as rnd
+
+import numpy as np
 
 
 def f(x):
@@ -14,16 +17,55 @@ def calc_integ(f, a, b, exp_nmb):
 
     for i in range(exp_nmb):
         p = rnd.random()
-        x = (xmax - xmin) * p + xmin
+        xp = (xmax - xmin) * p + xmin
         p = rnd.random()
-        y = (ymax - ymin) * p + ymin
-        if f(x) > y:
+        yp = (ymax - ymin) * p + ymin
+        if f(xp) > yp:
             m += 1
     return m * (b - a) * f(b) / exp_nmb
 
 
+def create_series() -> np.ndarray:
+    series = np.empty(5)
+    i = 0
+    for power in range(3, 8):
+        series[i] = calc_integ(f, 0, 2, 10 ** power)
+        i += 1
+    return series
+
+
+def calc_eps(seria) -> float:
+    return math.fabs((seria - 6) / 6)
+
+
+def create_eps(series):
+    eps = np.empty(5, dtype=np.ndarray)
+    ind = 0
+    for s in series:
+        cur_eps = calc_eps(s)
+        eps[ind] = np.array(cur_eps)
+        ind += 1
+    return eps
+
+
 def main():
-    print(calc_integ(f, 2, 0, 10 ** 2))
+    series = create_series()
+    eps = create_eps(series)
+    sums = np.empty(5)
+    eps_sums = np.empty(5)
+
+    for i in range(len(series)):
+        sums[i] = sum(series) / len(series)
+        eps_sums[i] = math.fabs((sums[i] - 6) / 6)
+
+    for i in range(5):
+        print(f"series{i}:{series[i]}")
+        for e in eps:
+            print(f"eps{i}:{e}")
+        for s in sums:
+            print(f"sums {i}:{s}")
+        for e in eps_sums:
+            print(f"eps sums{i}:{e}")
 
 
 if __name__ == "__main__":
