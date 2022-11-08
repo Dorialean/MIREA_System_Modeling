355d98b3772c643a08ef6274a21ae5a56c34ffbc
Dorialean <lana.lakutina@mail.ru>
2022-11-08 13:27:08 +0300

    1st task from 3rd homework

--- normal_law.py
+++ normal_law.py
@@ -2,42 +2,41 @@ import math
 import random
 from scipy import integrate
 import numpy as np
-
 import matplotlib.pyplot as plt
 
 
-def probability_density_distribution(x, M, st_deviation):
-    return math.e ** (-((x - M) ** 2) / (2 * st_deviation ** 2)) / (st_deviation * math.sqrt(2 * math.pi))
-
-
-def normal_law(x, f):
-    v, err = integrate.quad(f, -math.inf, x)
-    return v
+def rapsredelenie_plotnosti_veroyatnosti(x, sigma, M):
+    px = (1 / (sigma * math.sqrt(2 * math.pi))) * math.exp(-(x - M) ** 2 / (2 * (sigma ** 2)))
+    return px
 
 
 def main():
-    M_1 = 10
-    st_deviation_1 = 2
-    x_1 = random.random()
-    res_1 = probability_density_distribution(x_1, M_1, st_deviation_1)
+    X = range(0, 21, 2)
+    y1 = []
+    y2 = []
+    y3 = []
+    y4 = []
+    Y = [y1, y2, y3, y4]
+    sigma = 0
 
-    M_2 = 10
-    st_deviation_2 = 1
-    x_2 = random.random()
-    res_2 = probability_density_distribution(x_2, M_2, st_deviation_2)
+    for y in Y:
+        M = 10
+        if y == y1:
+            sigma = 2
+        elif y == y2:
+            sigma = 1
+        elif y == y3:
+            sigma = 0.5
+        elif y == y4:
+            M = 12
+            sigma = 1
 
-    M_3 = 10
-    st_deviation_3 = 1 / 2
-    x_3 = random.random()
-    res_3 = probability_density_distribution(x_3, M_3, st_deviation_3)
+        for i in range(len(X)):
+            y.append(rapsredelenie_plotnosti_veroyatnosti(X[i], sigma, M))
 
-    M_4 = 12
-    st_deviation_4 = 1
-    x_4 = random.random()
-    res_4 = probability_density_distribution(x_4, M_4, st_deviation_4)
+        plt.plot(X, y)
 
-    # Нужно построить графики (1 задание)
-
+    plt.show()
 
 if __name__ == '__main__':
     main()
